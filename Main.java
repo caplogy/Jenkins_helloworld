6fd95eb6862c496cb4788fd598939ce24d0e219a
billel mezaour <116521886+caplogy@users.noreply.github.com>
2023-11-15 23:44:35 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, c'est billel !");
+        System.out.println("Hello ECE, c'est billel !");
     }
 }
